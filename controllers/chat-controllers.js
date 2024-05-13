f67d5bffa09790b3f99f1a10a08464a73b4535d0
Henry <henrywl@gmail.com>
2024-05-13 17:55:26 +0100

    removed old example data

--- controllers/chat-controllers.js
+++ controllers/chat-controllers.js
@@ -1,38 +1,6 @@
 // require model
 const Chat = require("../models/chat");
 
-const chatList = [
-  {
-    id: "1",
-    users: ["uid1, uid2"],
-    messages: [
-      {
-        messageId: "m1",
-        text: "hello!",
-        // each message will also have a timestamp
-      },
-      {
-        messageId: "m2",
-        text: "hello to you too!",
-      },
-    ],
-  },
-  {
-    id: "2",
-    users: ["uid1, uid2"],
-    messages: [
-      {
-        messageId: "m3",
-        text: "Wow this is cool!",
-      },
-      {
-        messageId: "m4",
-        text: "I know right!",
-      },
-    ],
-  },
-];
-
 const getAllChats = async (req, res, next) => {
   // need to change this to getAllChats for logged in user
   // e.g. getAllChatsForUserID or getAllChatsForLoggedInUser

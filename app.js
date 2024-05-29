08ff03178a32f3c3dc636ab77091854f5c94a1c7
Henry <henrywl@gmail.com>
2024-05-29 01:22:11 +0100

    using headers as well

--- app.js
+++ app.js
@@ -21,15 +21,15 @@ app.use(cors({
 
 app.use(bodyParser.json())
 
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//   );
-//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, PUT");
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, PUT");
+  next();
+});
 
 // app.use(cors())
 

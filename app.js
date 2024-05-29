67b6f2403d8e825b2ef21bde2fa23dbe54c2546b
Henry <henrywl@gmail.com>
2024-05-29 01:17:41 +0100

    moved cors line

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ dotenv.config()
 const usersRoutes = require('./routes/users-routes');
 const chatRoutes = require('./routes/chat-routes');
 
-app.use(cors())
+
 
 // app.use(cors({
 //   origin: 'https://chatme-frontend-jye8q8otj-henrys-projects-989000f2.vercel.app',
@@ -30,6 +30,8 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, PUT");
   next();
 });
+
+app.use(cors())
 
 
 // app.get('/', (req, res) => {
